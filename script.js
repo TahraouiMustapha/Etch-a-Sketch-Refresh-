3a6069dd0bb476aa3e2117ca975e23dffd1ce5d0
mustapha <moustaphaoussama1818@gmail.com>
2024-10-16 20:41:52 +0100

    feat : adding a btn that will ask the user for the number of squares for the new grid, and make it

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const container = document.querySelector('.container');
+const newGridBtn = document.querySelector('.new-grid-btn');
 
-makeGrid();
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-        square.classList.add('colored'); 
-    })
+makeGrid(16);
+
+newGridBtn.addEventListener('click', () => {
+    let newNumber;
+    do {
+        newNumber = Number(prompt("Enter a new number for the Grid\n (Your choice < 100)", "16"));
+    } while (newNumber > 100);
+    removeGrid();   
+    makeGrid(newNumber);
 })
 
 
-
-function createSquare() {
+function createSquare(nmbSquares) {
     const div = document.createElement('div');
     div.classList.add('square');
+    div.style.width = `calc(100% / ${nmbSquares})`;
+    div.style.height = `calc(100% / ${nmbSquares})`;
     return div;
 }
 
-function makeGrid() {
-    for (let i=0; i< 16 * 16; i++) {
-        container.appendChild(createSquare())
-    }    
+function makeGrid(nmbSquares) {
+    for (let i=0; i< nmbSquares * nmbSquares; i++) {
+        container.appendChild(createSquare(nmbSquares))
+    }  
+    
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('colored'); 
+        })
+    })
+}
+
+function removeGrid () {
+    container.textContent = '';
 }
